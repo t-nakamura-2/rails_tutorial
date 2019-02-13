934a43046e63967c95a6a036cdc9e9d07000ab25
t_nakamura2 <t_nakamura2@AMBP10813.local>
2019-02-13 19:58:00 +0900

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ end
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.6'
+
+gem 'bootstrap-sass', '3.3.7'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3', '~> 1.3.6'
 # Use Puma as the app server
@@ -56,3 +58,5 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'rails-controller-testing'
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
 
-  resources :microposts
-  resources :users
-  root 'users#index'
+  root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/login', to: 'static_pages#login'
+  get  '/signup',  to: 'users#new'
+
+  root 'users#new'
+  get  'users/new',    to: 'users#new'
+
+  root 'microposts#home'
+
 end
