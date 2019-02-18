b80641fc7a96d68edb1052a8d2d31d1312ff3ba1
t_nakamura2 <t_nakamura2@AMBP10813.local>
2019-02-18 17:44:11 +0900

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,12 @@ Rails.application.routes.draw do
   get  '/contact', to: 'static_pages#contact'
   get  '/login', to: 'static_pages#login'
   get  '/signup',  to: 'users#new'
+  post '/signup', to:'users#create'
 
   root 'users#new'
   get  'users/new',    to: 'users#new'
+
+  resources :users
 
   root 'microposts#home'
 
